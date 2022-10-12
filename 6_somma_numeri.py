59213b31070ff12318d7a9a4e0982c51d08f4cfc
KarlosPenafiel <s-penafielmorales.karlos@isiskeynes.it>
2022-10-12 16:33:02 +0100

    [ADD]Creato il mio primo programma

---
+++ 6_somma_numeri.py
@@ -0,0 +1,6 @@
+primo= int(input ("inserire primo numero: "))
+secondo = int(input ("inserire secondo numero: "))
+terzo = int(input ("inserire terzo numero: "))
+if (primo+secondo+terzo) > 10 :
+    if primo>secondo and primo>terzo
+
